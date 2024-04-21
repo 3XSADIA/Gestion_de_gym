7066299805b315483fa0304c4d443dbe62d8b15c
salimi soukaina <salimisoukaina98@gmail.com>
2024-04-21 12:25:07 +0100

    version 5

---
+++ gestion_de_gym_v5/src/View/coachView.java
@@ -0,0 +1,426 @@
+package View;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class coachView extends JFrame implements ActionListener, KeyListener {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField search_tf;
+	private JTextField Nom_tf;
+	private JTextField Prenom_tf;
+	private JTextField NumTelephone_tf;
+	private JTextField Mission_tf;
+	private JTextField Id_tf;
+	private JTextField Salaire_tf;
+	private JTable table;
+	private JButton btnExit;
+	private JButton btnDelete;
+	private JButton btnAdd;
+	private JButton btnUpdate;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					coachView frame = new coachView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public coachView() {
+		setAutoRequestFocus(false);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1166, 537);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(185, 230, 242));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 255));
+		panel_1.setBounds(205, 50, 947, 450);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+
+		btnAdd = new JButton("Add");
+		btnAdd.setFont(new Font("Californian FB", Font.PLAIN, 19));
+		btnAdd.setBackground(new Color(87, 205, 230));
+		btnAdd.setSelectedIcon(new ImageIcon(coachView.class.getResource("/img/img2.png")));
+		btnAdd.setBounds(175, 380, 103, 45);
+		panel_1.add(btnAdd);
+
+		Id_tf = new JTextField();
+		Id_tf.setFont(new Font("Californian FB", Font.PLAIN, 21));
+		Id_tf.setBounds(10, 11, 317, 43);
+		panel_1.add(Id_tf);
+		Id_tf.setColumns(10);
+
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(255, 255, 255));
+		panel_3.setBorder(new MatteBorder(2, 2, 2, 2, new Color(128, 216, 238)));
+		panel_3.setBounds(10, 69, 425, 300);
+		panel_1.add(panel_3);
+		panel_3.setLayout(null);
+
+		JLabel lblNewLabel_2_1 = new JLabel("Nom\r\n");
+		lblNewLabel_2_1.setBounds(22, 57, 41, 24);
+		panel_3.add(lblNewLabel_2_1);
+		lblNewLabel_2_1.setFont(new Font("Californian FB", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_2 = new JLabel("Id");
+		lblNewLabel_2.setBounds(22, 11, 17, 24);
+		panel_3.add(lblNewLabel_2);
+		lblNewLabel_2.setFont(new Font("Californian FB", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_2_2 = new JLabel("Prenom\r\n");
+		lblNewLabel_2_2.setBounds(22, 101, 88, 31);
+		panel_3.add(lblNewLabel_2_2);
+		lblNewLabel_2_2.setFont(new Font("Californian FB", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_2_4 = new JLabel("NumTelephone");
+		lblNewLabel_2_4.setBounds(22, 154, 138, 31);
+		panel_3.add(lblNewLabel_2_4);
+		lblNewLabel_2_4.setFont(new Font("Californian FB", Font.PLAIN, 20));
+
+		search_tf = new JTextField();
+		search_tf.setFont(new Font("Californian FB", Font.PLAIN, 17));
+		search_tf.setBounds(188, 12, 138, 24);
+		panel_3.add(search_tf);
+		search_tf.setColumns(10);
+
+		Nom_tf = new JTextField();
+		Nom_tf.setFont(new Font("Californian FB", Font.PLAIN, 17));
+		Nom_tf.setBounds(188, 58, 138, 24);
+		panel_3.add(Nom_tf);
+		Nom_tf.setColumns(10);
+
+		Prenom_tf = new JTextField();
+		Prenom_tf.setFont(new Font("Californian FB", Font.PLAIN, 17));
+		Prenom_tf.setBounds(188, 105, 138, 24);
+		panel_3.add(Prenom_tf);
+		Prenom_tf.setColumns(10);
+
+		NumTelephone_tf = new JTextField();
+		NumTelephone_tf.setFont(new Font("Californian FB", Font.PLAIN, 17));
+		NumTelephone_tf.setBounds(188, 157, 138, 27);
+		panel_3.add(NumTelephone_tf);
+		NumTelephone_tf.setColumns(10);
+
+		Mission_tf = new JTextField();
+		Mission_tf.setFont(new Font("Californian FB", Font.PLAIN, 17));
+		Mission_tf.setBounds(188, 212, 138, 27);
+		panel_3.add(Mission_tf);
+		Mission_tf.setColumns(10);
+
+		JLabel lblNewLabel_2_3 = new JLabel("Mission");
+		lblNewLabel_2_3.setBounds(22, 209, 138, 31);
+		panel_3.add(lblNewLabel_2_3);
+		lblNewLabel_2_3.setFont(new Font("Californian FB", Font.PLAIN, 20));
+
+		JLabel lblNewLabel_2_3_1 = new JLabel("Salaire");
+		lblNewLabel_2_3_1.setFont(new Font("Californian FB", Font.PLAIN, 20));
+		lblNewLabel_2_3_1.setBounds(21, 258, 138, 31);
+		panel_3.add(lblNewLabel_2_3_1);
+
+		Salaire_tf = new JTextField();
+		Salaire_tf.setFont(new Font("Californian FB", Font.PLAIN, 17));
+		Salaire_tf.setColumns(10);
+		Salaire_tf.setBounds(188, 261, 138, 27);
+		panel_3.add(Salaire_tf);
+
+		btnDelete = new JButton("Delete");
+		btnDelete.setFont(new Font("Californian FB", Font.PLAIN, 19));
+		btnDelete.setBackground(new Color(58, 204, 211));
+		btnDelete.setBounds(310, 380, 103, 45);
+		panel_1.add(btnDelete);
+
+		btnUpdate = new JButton("Update");
+		btnUpdate.setFont(new Font("Californian FB", Font.PLAIN, 19));
+		btnUpdate.setBackground(new Color(87, 205, 230));
+		btnUpdate.setBounds(445, 380, 103, 45);
+		panel_1.add(btnUpdate);
+
+		btnExit = new JButton("Exit");
+		btnExit.setFont(new Font("Californian FB", Font.PLAIN, 19));
+		btnExit.setBackground(new Color(58, 204, 211));
+		btnExit.setBounds(583, 380, 103, 45);
+		panel_1.add(btnExit);
+
+		JLabel lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setIcon(new ImageIcon(coachView.class.getResource("/img/search-24.png")));
+		lblNewLabel_6.setBounds(330, 11, 30, 43);
+		panel_1.add(lblNewLabel_6);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(445, 69, 492, 300);
+		panel_1.add(scrollPane);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "Id", "Nom", "Prenom", "NumTelephone", "Mission", "Salaire" }));
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.WHITE);
+		panel_2.setBounds(205, 0, 947, 47);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+
+		JLabel lblNewLabel_1_1 = new JLabel("");
+		lblNewLabel_1_1.setIcon(new ImageIcon(coachView.class.getResource("/img/icons8-gym-64.png")));
+		lblNewLabel_1_1.setBounds(10, 0, 64, 42);
+		panel_2.add(lblNewLabel_1_1);
+
+		JLabel lblNewLabel_1 = new JLabel("Gym managment system");
+		lblNewLabel_1.setFont(new Font("Californian FB", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel_1.setForeground(new Color(99, 207, 235));
+		lblNewLabel_1.setBounds(84, 11, 275, 25);
+		panel_2.add(lblNewLabel_1);
+
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(0, 0, 201, 500);
+		contentPane.add(layeredPane);
+
+		JPanel panel_4_1_1 = new JPanel();
+		panel_4_1_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new equipementView().setVisible(true);
+			}
+		});
+		panel_4_1_1.setLayout(null);
+		panel_4_1_1.setForeground(Color.WHITE);
+		panel_4_1_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255),
+				new Color(255, 255, 255), new Color(255, 255, 255), new Color(255, 255, 255)));
+		panel_4_1_1.setBackground(new Color(58, 204, 211));
+		panel_4_1_1.setBounds(10, 444, 181, 42);
+		layeredPane.add(panel_4_1_1);
+
+		JLabel Statistiques_1_1_1 = new JLabel("Gestion Equipement");
+		Statistiques_1_1_1.setForeground(Color.WHITE);
+		Statistiques_1_1_1.setFont(new Font("Californian FB", Font.BOLD | Font.ITALIC, 20));
+		Statistiques_1_1_1.setBounds(0, 11, 171, 26);
+		panel_4_1_1.add(Statistiques_1_1_1);
+
+		JPanel panel_4_1 = new JPanel();
+		panel_4_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new paimentView().setVisible(true);
+			}
+		});
+		panel_4_1.setLayout(null);
+		panel_4_1.setForeground(Color.WHITE);
+		panel_4_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(255, 255, 255),
+				new Color(255, 255, 255), new Color(255, 255, 255)));
+		panel_4_1.setBackground(new Color(58, 204, 211));
+		panel_4_1.setBounds(10, 338, 181, 42);
+		layeredPane.add(panel_4_1);
+
+		JLabel Statistiques_1_1 = new JLabel("Gestion Seances");
+		Statistiques_1_1.setForeground(Color.WHITE);
+		Statistiques_1_1.setFont(new Font("Californian FB", Font.BOLD | Font.ITALIC, 21));
+		Statistiques_1_1.setBounds(20, 11, 161, 26);
+		panel_4_1.add(Statistiques_1_1);
+
+		JPanel panel_6 = new JPanel();
+		panel_6.setLayout(null);
+		panel_6.setForeground(Color.WHITE);
+		panel_6.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(255, 255, 255),
+				new Color(255, 255, 255), new Color(255, 255, 255)));
+		panel_6.setBackground(new Color(87, 205, 230));
+		panel_6.setBounds(10, 391, 181, 42);
+		layeredPane.add(panel_6);
+
+		JLabel lblGestionPaiement = new JLabel("Gestion Paiement");
+		lblGestionPaiement.setForeground(Color.WHITE);
+		lblGestionPaiement.setFont(new Font("Californian FB", Font.BOLD | Font.ITALIC, 21));
+		lblGestionPaiement.setBounds(10, 11, 171, 26);
+		panel_6.add(lblGestionPaiement);
+
+		JPanel panel_5 = new JPanel();
+		panel_5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new coachView().setVisible(true);
+			}
+		});
+		panel_5.setLayout(null);
+		panel_5.setForeground(Color.WHITE);
+		panel_5.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(255, 255, 255),
+				new Color(255, 255, 255), new Color(255, 255, 255)));
+		panel_5.setBackground(new Color(87, 205, 230));
+		panel_5.setBounds(10, 285, 181, 42);
+		layeredPane.add(panel_5);
+
+		JLabel lblGestionCoach = new JLabel("Gestion Coach\r\n");
+		lblGestionCoach.setForeground(Color.WHITE);
+		lblGestionCoach.setFont(new Font("Californian FB", Font.BOLD | Font.ITALIC, 21));
+		lblGestionCoach.setBounds(21, 11, 139, 26);
+		panel_5.add(lblGestionCoach);
+
+		JPanel panel_4 = new JPanel();
+		panel_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					new clientView().setVisible(true);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+
+		});
+		panel_4.setLayout(null);
+		panel_4.setForeground(Color.WHITE);
+		panel_4.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(255, 255, 255),
+				new Color(255, 255, 255), new Color(255, 255, 255)));
+		panel_4.setBackground(new Color(58, 204, 211));
+		panel_4.setBounds(10, 232, 181, 42);
+		layeredPane.add(panel_4);
+
+		JLabel Statistiques_1 = new JLabel("Gestion Clients");
+		Statistiques_1.setForeground(Color.WHITE);
+		Statistiques_1.setFont(new Font("Californian FB", Font.BOLD | Font.ITALIC, 21));
+		Statistiques_1.setBounds(20, 11, 151, 26);
+		panel_4.add(Statistiques_1);
+
+		JPanel panel = new JPanel();
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new home().setVisible(true);
+			}
+
+		});
+		panel.setBackground(new Color(87, 205, 230));
+		panel.setForeground(new Color(255, 255, 255));
+		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(255, 255, 255), new Color(255, 255, 255),
+				new Color(255, 255, 255), new Color(255, 255, 255)));
+		panel.setBounds(10, 179, 181, 42);
+		layeredPane.add(panel);
+		panel.setLayout(null);
+
+		JLabel Statistiques = new JLabel("Statistiques");
+		Statistiques.setBounds(34, 11, 107, 26);
+		panel.add(Statistiques);
+
+		Statistiques.setForeground(new Color(255, 255, 255));
+		Statistiques.setFont(new Font("Californian FB", Font.BOLD | Font.ITALIC, 21));
+
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon(coachView.class.getResource("/img/imageedit_1_8651314769 (1).png")));
+		lblNewLabel_8.setBounds(0, 24, 178, 144);
+		layeredPane.add(lblNewLabel_8);
+
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(coachView.class.getResource("/img/img2.png")));
+		lblNewLabel.setBounds(-50, 0, 251, 500);
+		layeredPane.add(lblNewLabel);
+	}
+
+	public int getSearchId() {
+		String idText = search_tf.getText();
+		if (idText.isEmpty()) {
+			// Handle the case where the text field is empty
+			return -1; // Or any other appropriate default value
+		}
+
+		try {
+			int id = Integer.parseInt(idText);
+			return id;
+		} catch (NumberFormatException e) {
+			// Handle the case where the text field contains non-numeric characters
+			e.printStackTrace(); // Or log the error
+			return -1; // Or any other appropriate default value
+		}
+	}
+
+	public void clearInputs() {
+		Id_tf.setText("");
+		Nom_tf.setText("");
+		Prenom_tf.setText("");
+		NumTelephone_tf.setText("");
+		Mission_tf.setText("");
+		Salaire_tf.setText("");
+
+	}
+
+	public void addAddButtonListener(ActionListener listener) {
+		btnAdd.addActionListener(listener);
+	}
+
+	public void addUpdateButtonListener(ActionListener listener) {
+		btnUpdate.addActionListener(listener);
+	}
+
+	public void addDeleteButtonListener(ActionListener listener) {
+		btnDelete.addActionListener(listener);
+	}
+
+	public void addExitButtonListener(ActionListener listener) {
+		btnExit.addActionListener(listener);
+	}
+
+	public void addSearcheButtonListener(KeyAdapter kA) {
+		btnDelete.addKeyListener(kA);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+}
